e34a5a6163f374a810012c1becc635005e3fa035
philipyao <187239625@qq.com>
2017-12-15 21:08:23 +0800

    x

---
+++ src/base/def/def.go
@@ -0,0 +1,16 @@
+package def
+
+//服务器类型定义
+const (
+    ServerTypePlatsvr               = 1
+    ServerTypePaysvr                = 2
+    ServerTypeConfsvr               = 3
+
+    ServerTypeRanksvr               = 21
+    ServerTypeReplaysvr             = 22
+
+    ServerTypeGatesvr               = 51
+    ServerTypeGamesvr               = 52
+    ServerTypeSessionsvr            = 53
+    ServerTypeDBsvr                 = 54
+)
