7d48fdff772bf31ea226eb4e5127c493eb04b22b
philipyao <187239625@qq.com>
2018-01-05 22:29:20 +0800

    xx

--- src/project/world/ranksvr/main.go
+++ src/project/world/ranksvr/main.go
@@ -4,16 +4,16 @@ import (
     "base/srv"
 )
 
-const (
-    rpcName     = "Rank"
-)
-
 func main() {
+
+    //handle rpc
     name, worker := NewRpc()
     err := srv.HandleRpc(name, worker)
     if err != nil {
         panic(err)
     }
+
+    //start run
     srv.Run()
 }
 
