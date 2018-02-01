20d20b4fde2cb04814c7771f5c929b02a3fced96
Your Name <you@example.com>
2018-02-01 15:16:53 +0800

    xx

--- src/project/public/confsvr/conf_zk.go
+++ src/project/public/confsvr/conf_zk.go
@@ -52,7 +52,15 @@ func attachWithZK(namespace, key string) error {
     }
     log.Debug("attach: %v %v", namespace, key)
     configPath = strings.Join([]string{commdef.ZKPrefixConfig, namespace, key}, "/")
-    return conn.Write(configPath, []byte{})
+    exist, err = conn.Exists(configPath)
+    if err != nil {
+        return err
+    }
+    if !exist {
+        log.Info("init config path: %v", configPath)
+        return conn.Write(configPath, []byte{})
+    }
+    return nil
 }
 
 //pub变更消息给ZK
